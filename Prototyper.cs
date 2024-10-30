08a8f47745942912bd79484f4bd134a792035460
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-10-30 12:57:46 -0400

    Implementation for FlexElement type binding / switching element type

---
+++ Prototyper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlocks
+{
+    public static class Prototyper
+    {
+        /// <summary>
+        /// Create prototype objects (default initialized) for all derived types of the specified Type.
+        /// </summary>
+        public static IEnumerable<T> PrototypeDerivedTypes<T>()
+        where T : IClonable<T>
+        {
+            Type baseType = typeof(T);
+            return baseType.Assembly.GetTypes()
+                                    .Where(t => t.IsSubclassOf(baseType) && !t.IsAbstract)
+                                    .Select(t => (T?)Activator.CreateInstance(t) 
+                                                 ?? throw new Exception("Failed to create Prototypes"));
+
+        }
+    }
+}
