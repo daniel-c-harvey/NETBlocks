8d2a600db19ce77d969df0583169ae5f9cf8bf04
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-10-31 16:36:28 -0400

    Popup -> Modal & linkage

---
+++ ConfirmEventArgs.cs
@@ -0,0 +1,16 @@
+﻿
+namespace NetBlocks
+{
+    public delegate void ConfirmEventHandler<T>(object sender, ConfirmEventArgs<T> e);
+
+    public class ConfirmEventArgs<T> : EventArgs
+    {
+        public T NewValue { get; set; }
+        public bool Confirm { get; set; } = false;
+
+        public ConfirmEventArgs(T newValue)
+        {
+            NewValue = newValue;
+        }
+    }
+}
