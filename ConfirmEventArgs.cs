66362ae750f606b3aa9744806655df43c3200bb6
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-11-04 11:29:53 -0500

    Navigation and Modal -> Confirmation refactors part 1

--- ConfirmEventArgs.cs
+++ ConfirmEventArgs.cs
@@ -1,16 +1,23 @@
 ﻿
-namespace NetBlocks
+namespace Core
 {
-    public delegate void ConfirmEventHandler<T>(object sender, ConfirmEventArgs<T> e);
-
-    public class ConfirmEventArgs<T> : EventArgs
+    public delegate void ConfirmEventHandler(object sender, ConfirmEventArgs e);
+    public class ConfirmEventArgs : EventArgs
     {
-        public T NewValue { get; set; }
-        public bool Confirm { get; set; } = false;
-
-        public ConfirmEventArgs(T newValue)
-        {
-            NewValue = newValue;
-        }
+        public static ConfirmEventArgs Confirm = new() { IsConfirmed = true };
+        public static ConfirmEventArgs Deny = new() { IsConfirmed = false };
+        
+        public bool IsConfirmed { get; set; } = false;
     }
+
+    //public delegate void ConfirmEventHandler<T>(object sender, ConfirmEventArgs<T> e);
+    //public class ConfirmEventArgs<T> : ConfirmEventArgs
+    //{
+    //    public T NewValue { get; set; }
+
+    //    public ConfirmEventArgs(T newValue)
+    //    {
+    //        NewValue = newValue;
+    //    }
+    //}
 }
