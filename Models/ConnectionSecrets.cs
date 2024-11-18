c21ed6d46c6e36599dab46fd6324ef8ba98c4e6c
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-11-18 18:28:12 -0500

    Connection Secrets fixes

--- Models/ConnectionSecrets.cs
+++ Models/ConnectionSecrets.cs
@@ -2,13 +2,14 @@
 {
     public class ConnectionSecrets
     {
-        public string ActiveConnection { get; set; } = default!;
+        public string ActiveConnectionName { get; set; } = default!;
         public IEnumerable<Connection> Connections { get; set; } = default!;
     }
 
     public class Connection
     {
-        public string Name { get; set; } = default!;
+        public string ConnectionName { get; set; } = default!;
+        public string DatabaseName {  get; set; } = default!;
         public string ConnectionString { get; set; } = default!;
     }
 }
