3e98e341c005c4e746e16e0cf4e101d4ae4ef7b1
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2024-11-22 12:43:22 -0500

    WIP Edit Flex Image  - Image Chooser implementation, working better on chrome.  the dataURL might be problematic on FF  - EditFlexImageViewModel needs work

--- Models/MIME.cs
+++ Models/MIME.cs
@@ -12,5 +12,15 @@
             { ".svg", "image/svg+xml" },
             { ".bmp", "image/bmp" }
         };
+
+        public static readonly Dictionary<string, string> EXTENSIONS = new()
+        {
+            { "image/jpeg", ".jpg" },
+            { "image/png", ".png" },
+            { "image/gif", ".gif" },
+            { "image/webp", ".webp" },
+            { "image/svg+xml", ".svg" },
+            { "image/bmp", ".bmp" }
+        };
     }
 }
