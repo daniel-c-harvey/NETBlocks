f30e142c0df0375a7be8502343076db72abd711f
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-01-14 00:08:34 -0500

    Image Loading File Input component can now internally validate and store the file, which the greater part of the UI bind to for access to proedurally generated URI
     flex element saves the proto choice for image but the image data is not sent to the server for saving yet, nor are stored images retrieved yet based on the URI

---
+++ Models/MessageEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace NetBlocks.Models;
+
+public class MessageEventArgs : EventArgs
+{
+    public string Message { get; }
+    
+    public MessageEventArgs(string message)
+    {
+        Message = message;
+    }
+}
+
+public delegate void MessageEventHandler(object sender, MessageEventArgs e);
