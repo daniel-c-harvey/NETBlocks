065d8e295ecc8d3985a60d85803cbade1952b796
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-01-15 11:29:25 -0500

    MediaBinary API (Client <-> Server) serialization support
    WIP FlexImage.Image Property MediaStorage attribute usage

--- Models/MediaBinary.cs
+++ Models/MediaBinary.cs
@@ -6,7 +6,10 @@ namespace NetBlocks.Models
         public string Base64 { get; set; } = default!;
         public long Size { get; set; } = default!;
         public string Extension { get; set; } = default!;
-
+        
+        // only for serializer
+        public MediaBinary() { }
+        
         public MediaBinary(MediaBinaryDto other) 
         {
             Base64 = Convert.ToBase64String(other.Bytes); // probably smart to make the API operate in base64 instead of byte arrays, it's much more compressed
