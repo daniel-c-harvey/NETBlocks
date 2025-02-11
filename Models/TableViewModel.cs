4ded9af997e7fb3ebe8ca7cf2dab0e5b5d24100d
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-02-11 10:09:57 -0500

    Edit Mode Overall Styling

---
+++ Models/TableViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBlocks.Models
+{
+    public interface IValue<T>
+    {
+        T Value { get; }
+    }
+
+    public interface IIndexed
+    {
+        int Index { get; }
+    }
+
+    public interface INew
+    {
+        bool IsNew { get; set; }
+    }
+    
+    public interface IDirty
+    {
+        bool IsDirty { get; set; }
+    }
+
+    public class TableViewModel<T> : IIndexed, INew, IDirty, IValue<T>
+    {
+        public int Index { get; }
+        public bool IsNew { get; set; } = false;
+        public bool IsDirty { get; set; } = false;
+        public T Value { get; set; }
+
+        public TableViewModel(T value, int index)
+        {
+            Value = value;
+            Index = index;
+        }
+    }
+}
