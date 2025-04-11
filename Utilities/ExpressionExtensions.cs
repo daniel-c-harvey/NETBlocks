a52b9d0e5aab72a264424d41130a3b2cb60ffb2f
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-04-11 08:56:43 -0400

    Composite Data Adapter Flesh, everything but paging is passing tests

--- Utilities/ExpressionExtensions.cs
+++ Utilities/ExpressionExtensions.cs
@@ -50,4 +50,113 @@ public static class ExpressionExtensions
             return node == _oldParameter ? _newParameter : base.VisitParameter(node);
         }
     }
+    
+    public static Expression<Func<T1, T2, bool>> And<T1, T2>(
+        this Expression<Func<T1, T2, bool>> left,
+        Expression<Func<T1, T2, bool>> right)
+    {
+        var parameter1 = Expression.Parameter(typeof(T1), "x");
+        var parameter2 = Expression.Parameter(typeof(T2), "y");
+        
+        var leftVisitor = new ReplaceParametersVisitor(
+            new[] { left.Parameters[0], left.Parameters[1] }, 
+            new[] { parameter1, parameter2 });
+        var leftExpr = leftVisitor.Visit(left.Body);
+        
+        var rightVisitor = new ReplaceParametersVisitor(
+            new[] { right.Parameters[0], right.Parameters[1] }, 
+            new[] { parameter1, parameter2 });
+        var rightExpr = rightVisitor.Visit(right.Body);
+        
+        var combinedExpr = Expression.AndAlso(leftExpr, rightExpr);
+        return Expression.Lambda<Func<T1, T2, bool>>(combinedExpr, parameter1, parameter2);
+    }
+
+    public static Expression<Func<T1, T2, bool>> Or<T1, T2>(
+        this Expression<Func<T1, T2, bool>> left,
+        Expression<Func<T1, T2, bool>> right)
+    {
+        var parameter1 = Expression.Parameter(typeof(T1), "x");
+        var parameter2 = Expression.Parameter(typeof(T2), "y");
+        
+        var leftVisitor = new ReplaceParametersVisitor(
+            new[] { left.Parameters[0], left.Parameters[1] }, 
+            new[] { parameter1, parameter2 });
+        var leftExpr = leftVisitor.Visit(left.Body);
+        
+        var rightVisitor = new ReplaceParametersVisitor(
+            new[] { right.Parameters[0], right.Parameters[1] }, 
+            new[] { parameter1, parameter2 });
+        var rightExpr = rightVisitor.Visit(right.Body);
+        
+        var combinedExpr = Expression.OrElse(leftExpr, rightExpr);
+        return Expression.Lambda<Func<T1, T2, bool>>(combinedExpr, parameter1, parameter2);
+    }
+    
+    public static Expression<Func<T1, T2, T3, bool>> And<T1, T2, T3>(
+        this Expression<Func<T1, T2, T3, bool>> left,
+        Expression<Func<T1, T2, T3, bool>> right)
+    {
+        var parameter1 = Expression.Parameter(typeof(T1), "x");
+        var parameter2 = Expression.Parameter(typeof(T2), "y");
+        var parameter3 = Expression.Parameter(typeof(T2), "z");
+        
+        var leftVisitor = new ReplaceParametersVisitor(
+            new[] { left.Parameters[0], left.Parameters[1], left.Parameters[2] }, 
+            new[] { parameter1, parameter2, parameter3 });
+        var leftExpr = leftVisitor.Visit(left.Body);
+        
+        var rightVisitor = new ReplaceParametersVisitor(
+            new[] { right.Parameters[0], right.Parameters[1], right.Parameters[2] }, 
+            new[] { parameter1, parameter2, parameter3 });
+        var rightExpr = rightVisitor.Visit(right.Body);
+        
+        var combinedExpr = Expression.AndAlso(leftExpr, rightExpr);
+        return Expression.Lambda<Func<T1, T2, T3, bool>>(combinedExpr, parameter1, parameter2,  parameter3);
+    }
+
+    public static Expression<Func<T1, T2, T3, bool>> Or<T1, T2, T3>(
+        this Expression<Func<T1, T2, T3, bool>> left,
+        Expression<Func<T1, T2, T3, bool>> right)
+    {
+        var parameter1 = Expression.Parameter(typeof(T1), "x");
+        var parameter2 = Expression.Parameter(typeof(T2), "y");
+        var parameter3 = Expression.Parameter(typeof(T2), "z");
+        
+        var leftVisitor = new ReplaceParametersVisitor(
+            new[] { left.Parameters[0], left.Parameters[1], left.Parameters[2] }, 
+            new[] { parameter1, parameter2, parameter3 });
+        var leftExpr = leftVisitor.Visit(left.Body);
+        
+        var rightVisitor = new ReplaceParametersVisitor(
+            new[] { right.Parameters[0], right.Parameters[1], right.Parameters[2] }, 
+            new[] { parameter1, parameter2, parameter3 });
+        var rightExpr = rightVisitor.Visit(right.Body);
+        
+        var combinedExpr = Expression.OrElse(leftExpr, rightExpr);
+        return Expression.Lambda<Func<T1, T2, T3, bool>>(combinedExpr, parameter1, parameter2,  parameter3);
+    }
+
+    public class ReplaceParametersVisitor : ExpressionVisitor
+    {
+        private readonly ParameterExpression[] _oldParameters;
+        private readonly ParameterExpression[] _newParameters;
+        
+        public ReplaceParametersVisitor(ParameterExpression[] oldParameters, ParameterExpression[] newParameters)
+        {
+            _oldParameters = oldParameters;
+            _newParameters = newParameters;
+        }
+        
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            for (int i = 0; i < _oldParameters.Length; i++)
+            {
+                if (node == _oldParameters[i])
+                    return _newParameters[i];
+            }
+            
+            return base.VisitParameter(node);
+        }
+    }
 };
