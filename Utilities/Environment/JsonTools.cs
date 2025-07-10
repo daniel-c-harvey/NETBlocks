56d4785a5c43009807307c48a90c4a3ce7aa8f80
Daniel Harvey <the.real.daniel.harvey@gmail.com>
2025-07-09 23:16:42 -0400

    configurable admin account

--- Utilities/Environment/JsonTools.cs
+++ Utilities/Environment/JsonTools.cs
@@ -2,5 +2,22 @@
 
 public class JsonTools<T>
 {
-    
+    public static T LoadFromFile(string filePathFromBase)
+    {
+        using var reader = new StreamReader(File.OpenRead(filePathFromBase));
+        
+        string json = reader.ReadToEnd();
+        var model = System.Text.Json.JsonSerializer.Deserialize<T>(json);
+        reader.Close();
+        return model ?? throw new Exception($"Could not read settings for {typeof(T).Name}");
+    }
+
+    public static void SaveToFile(string filePathFromBase, T model)
+    {
+        using var writer = new StreamWriter(File.Create(filePathFromBase));
+        
+        string json = System.Text.Json.JsonSerializer.Serialize(model);
+        writer.WriteLine(json);
+        writer.Close();
+    }
 }
