817ab0b30b4b308454fb8cb15aa8eff87e2cf430
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-08-29 09:18:41 -0400

    .

---
+++ Utilities/NTree.cs
@@ -0,0 +1,22 @@
+﻿namespace NetBlocks.Utilities;
+
+public class NTree<T>
+{
+    public List<Node> Roots { get; private set; }
+    
+    public class Node
+    {
+        public T? Value { get; set; }
+        public List<Node> Children { get; private set; } = [];
+    }
+
+    public NTree()
+    {
+        Roots = [];
+    }
+    
+    public NTree(List<Node> roots)
+    {
+        Roots = roots;
+    }
+}
