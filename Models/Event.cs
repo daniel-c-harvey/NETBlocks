ec6c9348ef204998b0463e49fb2e1d8a1e50faed
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-09-06 13:39:26 -0400

    Front End Work  - Redesign component wiring for audio playback  - Removed playback logic from the player control and moved it to injectable audio player engine service  - Chunked/buffered stream loading from Content API passed to Web Audio API playback in 8K blocks

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,4 +5,12 @@
         public delegate void EventBase();
         public delegate Task EventBaseAsync();
     }
+    
+    public static class Events
+    {
+        public delegate void Event();
+        public delegate void Event<T>(T args);
+        public delegate Task EventAsync();
+        public delegate Task EventAsync<T>(T args);
+    }
 }
